b389aec2bed02eac7e7900052da615552b14d6b1
Creeper <98064234+TheyCreeper@users.noreply.github.com>
2026-01-30 13:29:29 -0500

    feat: start phasing out of the SongViewModel

--- src/SlayerApp/Model/Playlist.cs
+++ src/SlayerApp/Model/Playlist.cs
@@ -1,6 +1,7 @@
 using Avalonia.Media;
 using SlayerApp.ViewModel;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 
@@ -8,7 +9,7 @@ public class Playlist
 {
     public string Name { get; set; } = string.Empty;
     public int SongCount { get; set; }
-    public List<SongViewModel> trackList { get; set; } = new List<SongViewModel>();
+    public ObservableCollection<Song> trackList { get; set; } = new ObservableCollection<Song>();
     public ImageDrawing playlistImage { get; set; }
     public string PlaylistImage { get; set; }
     public bool Shuffle { get; set; }
